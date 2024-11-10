323f73a33457364f7e485cb38b41e48a3b7f5c5c
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-10 19:54:24 +0500

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        String result = condition ? "Та-да-да-дам колобок съеден" : "Парам-парам-пам колобок сбежал";
+        System.out.println(result);
+    }
+}
