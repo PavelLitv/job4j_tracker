99ee481500dd29afddecc833ed825bfd4d9df79f
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-13 11:32:35 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #521511]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -55,8 +55,9 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, items.length - index - 1);
-            items[items.length - 1] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
             return true;
         }
         return false;
