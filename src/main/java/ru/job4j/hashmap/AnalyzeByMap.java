055c6a412a69eb290788dc4ec51a3d5d5617bd27
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-20 23:08:51 +0500

    4. Аттестация [#504878]

---
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -0,0 +1,84 @@
+package ru.job4j.hashmap;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double sum = 0.0;
+        int count = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                sum += subject.score();
+                count++;
+            }
+        }
+        return sum / count;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        double sum = 0.0;
+        int count = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                sum += subject.score();
+                count++;
+            }
+            labels.add(new Label(pupil.name(), sum / count));
+            sum = 0.0;
+            count = 0;
+        }
+        return labels;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        Map<String, Integer> scores = new LinkedHashMap<>();
+        double count = 0.0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                scores.put(
+                        subject.name(),
+                        scores.getOrDefault(subject.name(), 0) + subject.score()
+                );
+            }
+            count++;
+        }
+        for (Map.Entry<String, Integer> score : scores.entrySet()) {
+            labels.add(new Label(score.getKey(), score.getValue() / count));
+        }
+        return labels;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        int score = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                score += subject.score();
+            }
+            labels.add(new Label(pupil.name(), score));
+            score = 0;
+        }
+        labels.sort(Comparator.naturalOrder());
+        return labels.get(labels.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        List<Label> labels = new ArrayList<>();
+        Map<String, Integer> scores = new LinkedHashMap<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                scores.put(
+                        subject.name(),
+                        scores.getOrDefault(subject.name(), 0) + subject.score()
+                );
+            }
+        }
+        for (Map.Entry<String, Integer> score : scores.entrySet()) {
+            labels.add(new Label(score.getKey(), score.getValue()));
+        }
+        labels.sort(Comparator.naturalOrder());
+        return labels.get(labels.size() - 1);
+    }
+}
