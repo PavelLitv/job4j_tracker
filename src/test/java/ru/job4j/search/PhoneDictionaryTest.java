9a609643993b812fdc904200cb89868418edda8d
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-20 09:45:01 +0500

    4. Телефонный справочник на базе ArrayList [#41598 #522663]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -31,6 +31,26 @@ public class PhoneDictionaryTest {
     }
 
     @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Popov", "534873", "Petrograd")
+        );
+        ArrayList<Person> persons = phones.find("Popov");
+        assertThat(persons.get(0).getName()).isEqualTo("Ivan");
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Popov", "534873", "Petrograd")
+        );
+        ArrayList<Person> persons = phones.find("534");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Popov");
+    }
+
+    @Test
     public void whenFindThenEmpty() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
