502455f2dc0d8274db0f67fecec535c050a72947
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-23 19:42:23 +0500

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                if (i < left.split("/")[0].length()) {
+                    return Character.compare(right.charAt(i), left.charAt(i));
+                } else {
+                    return Character.compare(left.charAt(i), right.charAt(i));
+                }
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+}
